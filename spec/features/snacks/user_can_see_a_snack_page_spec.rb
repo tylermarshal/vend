d3cd36da09f9c7e1007ea5c90b2fdd29f3d7f82e
TMad <madsen.tyler@gmail.com>
2018-01-05 09:53:46 -0700

    add tests for snack show feature test

--- spec/features/snacks/user_can_see_a_snack_page_spec.rb
+++ spec/features/snacks/user_can_see_a_snack_page_spec.rb
@@ -7,10 +7,13 @@ describe "When a user visits a snacks page" do
         context "the user sees average price and count of different items for those machines" do
         it "sees the snacks attributes" do
           owner = Owner.create!(name: "Tyler")
-          machine = Machine.create!(location: "Mars", owner: owner)
-          machine = Machine.create!(location: "Earth", owner: owner)
-          snack1 = Snack.create!(name: "Beef Jerky", price: 5, machine: machine)
-          snack2 = Snack.create!(name: "Granola Bar", price: 3, machine: machine)
+          machine1 = Machine.create!(location: "Mars", owner: owner)
+          machine2 = Machine.create!(location: "Earth", owner: owner)
+          snack1 = Snack.create!(name: "Beef Jerky", price: 5)
+          snack2 = Snack.create!(name: "Granola Bar", price: 3)
+          SnackMachine.create!(machine: machine1, snack: snack1)
+          SnackMachine.create!(machine: machine2, snack: snack1)
+          SnackMachine.create!(machine: machine2, snack: snack2)
 
           visit machine_path(machine)
 
@@ -18,7 +21,12 @@ describe "When a user visits a snacks page" do
           expect(page).to have_content(snack1.price)
           expect(page).to_not have_content(snack2.name)
           expect(page).to_not have_content(snack2.price)
-          expect(page).to have_content(machine.average_price)
+          expect(page).to have_content("#{machine1.location} Vending Machine")
+          expect(page).to have_content(machine1.average_price)
+          expect(page).to have_content(machine1.count_unique_items)
+          expect(page).to have_content("#{machine2.location} Vending Machine")
+          expect(page).to have_content(machine1.average_price)
+          expect(page).to have_content(machine2.count_unique_items)
         end
       end
     end
